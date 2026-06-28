835b485e7958655cefef7b50674de10a6ee52dfd
ragul835 <ragulsiva369@zohomail.in>
2026-06-28 19:24:34 +0530

    content: change availability badge text to 'Accepting new clients'

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -171,7 +171,7 @@ export function Footer() {
                     <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                     <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
                   </span>
-                  <span>Available for new projects</span>
+                  <span>Accepting new clients</span>
                 </div>
               </li>
             </ul>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -177,7 +177,7 @@ function HeroSection() {
             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
             <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
           </span>
-          <span>Available for new projects</span>
+          <span>Accepting new clients</span>
         </div>
 
         {/* Headline */}
